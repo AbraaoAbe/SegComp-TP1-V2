73a214acc41e0bc0547ae1e8faff03a45b610756
AbraaoAbe <abraao.santos@edu.ufes.br>
2024-08-21 09:00:36 -0300

    telas criadas e integradas com o server.js, falta integração com a blockchain

--- front-end/server.js
+++ front-end/server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const path = require('path');
 const { exec } = require('child_process');
 
 const port = 3000;
 
 const requestHandler = (req, res) => {
+  const parsedUrl = url.parse(req.url, true); // Analisa a URL e obtém parâmetros de consulta
+  
     // FRONT END HTML GET
     if (req.method === 'GET' && req.url === '/') {
         const filePath = path.join(__dirname, 'index.html');
@@ -19,18 +22,14 @@ const requestHandler = (req, res) => {
             res.end(data);
             return;
         });
-    }
-
-    // Check if the request method is POST and the request URL is /generate
-    //GERAÇÃO E ASSINATURA DO CERTIFICADO
-    if (req.method === 'POST' && req.url === '/generate') {
+    } else if (req.method === 'POST' && req.url === '/generate') {
+      // Check if the request method is POST and the request URL is /generate
+      //GERAÇÃO E ASSINATURA DO CERTIFICADO
         let body = '';
-
         req.on('data', chunk => {
             // Append the chunk of data to the body variable
             body += chunk.toString();
         });
-
         req.on('end', () => {
             const data = JSON.parse(body);
             const textInput = data.textInput;
@@ -71,7 +70,43 @@ const requestHandler = (req, res) => {
                 res.end('400 Bad Request: Missing parameters');
             }
         });
-    } 
+    } else if (req.method === 'POST' && req.url === '/revoke'){
+      // Check if the request method is POST and the request URL is /revoke
+      //REVOCAÇÃO DO CERTIFICADO
+      let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            console.log("Dados recebidos para revogar:", data);
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Logica da revogação não implementada!');
+        });
+    } else if (req.method === 'GET' && parsedUrl.pathname === '/view') {
+      // Check if the request method is GET and the request URL is /view
+      //BUSCA DO CERTIFICADO
+      const certId = parsedUrl.query.certId; // Obtém o parâmetro certId
+
+        if (certId) {
+            // Aqui você pode buscar o certificado com o ID fornecido e retornar os dados
+            console.log("ID do certificado:", certId);
+
+            // Exemplo de resposta, substitua isso com sua lógica para buscar o certificado
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end('Logica da busca não implementada!');
+            // res.end(JSON.stringify({ certId: certId, status: 'Certificado encontrado!' }));
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('400 Bad Request: Certificado ID não fornecido!');
+        }
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    }
+
 };
 
 const server = http.createServer(requestHandler);
